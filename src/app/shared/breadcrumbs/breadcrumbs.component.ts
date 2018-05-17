65cdf3bb253e7e1476219523040b419674e202dc
Andres <Andres@MacBookProPancha.local>
2018-05-17 15:05:48 -0400

    Sección 8 terminada

--- src/app/shared/breadcrumbs/breadcrumbs.component.ts
+++ src/app/shared/breadcrumbs/breadcrumbs.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivationEnd } from '@angular/router';
+import 'rxjs/add/operator/filter';
+import 'rxjs/add/operator/map';
+import { Title, Meta, MetaDefinition } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-breadcrumbs',
@@ -7,7 +11,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BreadcrumbsComponent implements OnInit {
 
-  constructor() { }
+  labelPagina: string = '';
+  constructor(
+    private router: Router,
+    public title: Title,
+    public meta: Meta
+  ) {
+      this.getDataRoute().subscribe( data => {
+        this.labelPagina = data.titulo;
+        this.title.setTitle(this.labelPagina);
+        const metaTag: MetaDefinition = {
+          name: 'description',
+          content: this.labelPagina
+        };
+        this.meta.updateTag( metaTag );
+      });
+  }
+
+  getDataRoute() {
+    return this.router.events
+    .filter( evento => evento instanceof ActivationEnd )
+    .filter ( (evento: ActivationEnd) => evento.snapshot.firstChild === null )
+    .map( (evento: ActivationEnd) => evento.snapshot.data );
+  }
 
   ngOnInit() {
   }
